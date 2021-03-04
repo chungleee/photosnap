faa94f0f30303613ab80048005c43a3c75408e29
chungleee <leon.karho.chung@gmail.com>
2021-03-04 18:25:35 -0500

    button components + styling

---
+++ components/Buttons/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import RightArrow from '../Icons/RightArrow';
+import classes from './Button.module.scss';
+
+const Button = ({ children, variant }) => {
+	return (
+		<button
+			className={`${
+				!variant
+					? classes.button__default
+					: variant === 'white'
+					? classes.button__white
+					: variant === 'arrow'
+					? classes.button__default__arrow
+					: variant === 'black-arrow'
+					? classes.button__black__arrow
+					: ''
+			}`}
+		>
+			{children}
+
+			<RightArrow
+				className={`${
+					variant === 'arrow'
+						? classes.show
+						: variant === 'black-arrow'
+						? classes.show
+						: classes.hidden
+				}`}
+			/>
+		</button>
+	);
+};
+
+export default Button;
