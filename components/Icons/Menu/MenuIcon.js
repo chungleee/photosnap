09d8a9cebd64aae9fdad9f0e402a1a74dd7b1a02
chungleee <leon.karho.chung@gmail.com>
2021-03-05 17:44:56 -0500

    icon components

---
+++ components/Icons/Menu/MenuIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuIcon = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='15' height='10'>
+			<g fillRule='evenodd'>
+				<path d='M0 0h20v1H0zM0 5h20v1H0z' />
+			</g>
+		</svg>
+	);
+};
+export default MenuIcon;
