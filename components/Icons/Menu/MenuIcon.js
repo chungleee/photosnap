2056ea67a56be26cb556aa02ae374a1c88c1bcff
chungleee <leon.karho.chung@gmail.com>
2021-03-08 16:36:31 -0500

    accept classname prop

--- components/Icons/Menu/MenuIcon.js
+++ components/Icons/Menu/MenuIcon.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const MenuIcon = () => {
+const MenuIcon = ({ className }) => {
 	return (
-		<svg xmlns='http://www.w3.org/2000/svg' width='15' height='10'>
+		<svg
+			className={`${className ? className : ''}`}
+			xmlns='http://www.w3.org/2000/svg'
+			width='15'
+			height='10'
+		>
 			<g fillRule='evenodd'>
 				<path d='M0 0h20v1H0zM0 5h20v1H0z' />
 			</g>
