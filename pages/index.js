cc50995023f89a28d73735793efa2e1b25b71135
chungleee <leon.karho.chung@gmail.com>
2021-03-08 16:36:44 -0500

    testing header component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Container from '../components/Container/Container';
+import Header from '../components/Header/Header';
+
 const Home = () => {
 	return (
 		<div>
-			<h1>Home page</h1>
+			<Header />
 		</div>
 	);
 };
