a43930cbc4ffe4c3a3d20226c9513f1d53ded799
chungleee <leon.karho.chung@gmail.com>
2021-03-08 16:35:24 -0500

    add list menu & invite button with respective styling & menu icon toggle state

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Header.module.scss';
 import Container from '../Container/Container';
+import Button from '../Buttons/Button';
 import Logo from '../Icons/Logo/Logo';
 import MenuIcon from '../Icons/Menu/MenuIcon';
 import CloseIcon from '../Icons/Menu/CloseIcon';
 
 const Header = () => {
+	const [clicked, setClicked] = useState(false);
+
+	const handleToggleMenuIcon = () => {
+		setClicked(!clicked);
+	};
+
 	return (
 		<Container>
 			<header className={classes.header}>
 				<div>
 					<Logo />
 				</div>
-				<div>
-					<MenuIcon />
+				<div className={classes.menu}>
+					<ul>
+						<li>
+							<a href='#'>stories</a>
+						</li>
+						<li>
+							<a href='#'>features</a>
+						</li>
+						<li>
+							<a href='#'>pricing</a>
+						</li>
+					</ul>
+				</div>
+				<Button className={classes.invite__button}>get an invite</Button>
+				<div className={classes.menu__icons} onClick={handleToggleMenuIcon}>
+					{clicked ? <CloseIcon /> : <MenuIcon />}
 				</div>
 			</header>
 		</Container>
