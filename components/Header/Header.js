4b0ae672055d10482f1434c0619e1240abc871d4
chungleee <leon.karho.chung@gmail.com>
2021-03-09 18:40:38 -0500

    include menudropdown component with toggle logic

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,11 +5,12 @@ import Button from '../Buttons/Button';
 import Logo from '../Icons/Logo/Logo';
 import MenuIcon from '../Icons/Menu/MenuIcon';
 import CloseIcon from '../Icons/Menu/CloseIcon';
+import MenuDropdown from '../MenuDropdown/MenuDropdown';
 
 const Header = () => {
 	const [clicked, setClicked] = useState(false);
 
-	const handleToggleMenuIcon = () => {
+	const handleToggleDropdown = () => {
 		setClicked(!clicked);
 	};
 
@@ -32,10 +33,13 @@ const Header = () => {
 						</li>
 					</ul>
 				</div>
-				<Button className={classes.invite__button}>get an invite</Button>
-				<div className={classes.menu__icons} onClick={handleToggleMenuIcon}>
+				<span className={classes.invite__button}>
+					<Button>get an invite</Button>
+				</span>
+				<div className={classes.menu__icons} onClick={handleToggleDropdown}>
 					{clicked ? <CloseIcon /> : <MenuIcon />}
 				</div>
+				{clicked ? <MenuDropdown toggled={clicked} /> : null}
 			</header>
 		</Container>
 	);
