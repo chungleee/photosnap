a99af67e38214d115584f6c70f2bd845260874f2
chungleee <leon.karho.chung@gmail.com>
2021-03-15 12:41:48 -0400

    picture & source tag for dynamic hero image - add inner container classname for easy styling

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,13 +4,27 @@ import Container from '../Container/Container';
 import Button from '../Buttons/Button';
 import Accent from '../Accent/Accent';
 
-const Hero = ({ children, image, button }) => {
+const Hero = ({ children, images, button }) => {
 	return (
 		<div className={classes.hero}>
-			<img src={image.src} alt={image.alt} />
+			<picture>
+				<source
+					srcSet='/assets/home/tablet/create-and-share.jpg'
+					media='(min-width: 768px)'
+				/>
+				<source
+					srcSet='/assets/home/desktop/create-and-share.jpg'
+					media='(min-width: 1440px)'
+				/>
+
+				<img
+					src='/assets/home/mobile/create-and-share.jpg'
+					alt='Person standing on boardwalk by the lake'
+				/>
+			</picture>
 			<Container className={classes.container}>
-				<Accent className={classes.accent} />
-				<div>
+				<Accent className={classes.accent__rectangle} />
+				<div className={classes.inner__container}>
 					{children.map((child) => {
 						return (
 							<child.type key={child.props.children}>
