d0b47faea69c8aec3c13d2efb46f7721a3b72c2d
chungleee <leon.karho.chung@gmail.com>
2021-03-22 12:07:16 -0400

    storycard component: takes data from parents and displays images based on media query & headings & text

---
+++ components/StoryCard/StoryCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './StoryCard.module.scss';
+import Button from '../Buttons/Button';
+
+const StoryCard = ({ data }) => {
+	console.log('data: ', data);
+	return (
+		<div className={classes.storycard}>
+			<picture
+			// className={classes.storycard__image}
+			>
+				<source media='(min-width: 768px)' srcSet={data.images.tablet} />
+				<img
+					className={classes.storycard__image}
+					src={data.images.mobile}
+					alt={data.title}
+				/>
+			</picture>
+			<div className={classes.storycard__image__overlay}>
+				<div className={classes.storycard__description}>
+					<h3>{data.title}</h3>
+					<p>by {data.author}</p>
+					<Button className={classes.storycard__cta} variant='arrow'>
+						read story
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default StoryCard;
