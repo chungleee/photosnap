012fadce135d310cf1f5868ca322aa3959124f64
chungleee <leon.karho.chung@gmail.com>
2021-03-24 17:37:45 -0400

    array of pricing data - loop array to display pricing cards and passing data as props

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Container from '../components/Container/Container';
+import Grid from '../components/Grid/Grid';
 import Hero from '../components/Hero/Hero';
 import PricingCard from '../components/PricingCard/PricingCard';
 
@@ -8,6 +10,28 @@ const Pricing = () => {
 		tablet: '/assets/pricing/tablet/hero.jpg',
 		desktop: '/assets/pricing/desktop/hero.jpg',
 	};
+
+	const pricingData = [
+		{
+			plan: 'basic',
+			description:
+				'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+			price: '19.00',
+		},
+		{
+			plan: 'pro',
+			description:
+				'More advanced features available. Recommended for photography vetarans and professionals.',
+			price: '39.00',
+		},
+		{
+			plan: 'business',
+			description:
+				'Additional features available such as more detailed metrics. Recommended for business owners.',
+			price: '99.00',
+		},
+	];
+
 	return (
 		<div>
 			<Hero images={heroImages} alt='Photographer in sunset'>
@@ -18,6 +42,14 @@ const Pricing = () => {
 					photos.
 				</p>
 			</Hero>
+
+			<Container>
+				<Grid>
+					{pricingData.map((data) => {
+						return <PricingCard data={data} />;
+					})}
+				</Grid>
+			</Container>
 		</div>
 	);
 };
