577cc6a7e8b3bf6929a28d2a1b377aa77ae192fa
chungleee <leon.karho.chung@gmail.com>
2021-03-24 16:43:21 -0400

    feat: pricing page hero banner filled with its props

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Hero from '../components/Hero/Hero';
+import PricingCard from '../components/PricingCard/PricingCard';
 
 const Pricing = () => {
-	return <div>this is the pricing page</div>;
+	const heroImages = {
+		mobile: '/assets/pricing/mobile/hero.jpg',
+		tablet: '/assets/pricing/tablet/hero.jpg',
+		desktop: '/assets/pricing/desktop/hero.jpg',
+	};
+	return (
+		<div>
+			<Hero images={heroImages} alt='Photographer in sunset'>
+				<h1>pricing</h1>
+				<p>
+					Create all your stories. Photosnap is a platform for photographers and
+					visual storytellers. It's the simplest way to create and share your
+					photos.
+				</p>
+			</Hero>
+		</div>
+	);
 };
 
 export default Pricing;
