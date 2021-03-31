368f5b7df856206b308bf3dd2b263335ecb08a73
chungleee <leon.karho.chung@gmail.com>
2021-03-31 12:38:32 -0400

    feat: subscription toggle component

---
+++ components/Icons/Pricing/SubToggle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './SubToggle.module.scss';
+
+const SubToggle = () => {
+	return (
+		<div className={classes.outer}>
+			<div className={classes.inner}></div>
+		</div>
+	);
+};
+
+export default SubToggle;
