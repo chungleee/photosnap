b65bcf358da7547967ada29f20ef5a2193402c86
chungleee <leon.karho.chung@gmail.com>
2021-04-27 16:04:26 -0400

    style: include header container styles

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -16,8 +16,8 @@ const Header = () => {
 	};
 
 	return (
-		<Container>
-			<header className={classes.header}>
+		<header className={classes.header}>
+			<Container className={classes.header__container}>
 				<div>
 					<Logo />
 				</div>
@@ -46,8 +46,8 @@ const Header = () => {
 						handleToggleDropdown={handleToggleDropdown}
 					/>
 				) : null}
-			</header>
-		</Container>
+			</Container>
+		</header>
 	);
 };
 
