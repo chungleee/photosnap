e9c1c7dbdfc993b6f568625dbda23661e898001e
chungleee <leon.karho.chung@gmail.com>
2021-04-27 16:05:18 -0400

    feat: include Container component with custom styling

--- components/MenuDropdown/MenuDropdown.js
+++ components/MenuDropdown/MenuDropdown.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 import Button from '../Buttons/Button';
+import Container from '../Container/Container';
 import classes from './MenuDropdown.module.scss';
 
 const MenuDropdown = ({ toggled, handleToggleDropdown }) => {
@@ -10,21 +11,23 @@ const MenuDropdown = ({ toggled, handleToggleDropdown }) => {
 
 	return (
 		<div id='dropdown' className={`${classes.dropdown}`}>
-			<div className={classes.menu__dropdown}>
-				<ul>
-					<li onClick={handleToggleDropdown}>
-						<Link href='#'>stories</Link>
-					</li>
-					<li onClick={handleToggleDropdown}>
-						<Link href='#'>features</Link>
-					</li>
-					<li onClick={handleToggleDropdown}>
-						<Link href='/pricing'>pricing</Link>
-					</li>
-				</ul>
+			<Container className={classes.dropdown__container}>
+				<div className={classes.menu__dropdown}>
+					<ul>
+						<li onClick={handleToggleDropdown}>
+							<Link href='#'>stories</Link>
+						</li>
+						<li onClick={handleToggleDropdown}>
+							<Link href='#'>features</Link>
+						</li>
+						<li onClick={handleToggleDropdown}>
+							<Link href='/pricing'>pricing</Link>
+						</li>
+					</ul>
 
-				<Button className={classes.invite__button}>get an invite</Button>
-			</div>
+					<Button className={classes.invite__button}>get an invite</Button>
+				</div>
+			</Container>
 		</div>
 	);
 };
