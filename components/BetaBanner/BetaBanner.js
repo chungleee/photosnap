0f9537408d529674e2a5d50299f3123d7b5fa212
chungleee <leon.karho.chung@gmail.com>
2021-04-28 13:44:38 -0400

    beta banner component with styling

---
+++ components/BetaBanner/BetaBanner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './BetaBanner.module.scss';
+import Accent from '../Accent/Accent';
+import Button from '../Buttons/Button';
+
+const BetaBanner = () => {
+	return (
+		<div className={classes.beta}>
+			<picture>
+				<source
+					media='(min-width: 1440px)'
+					srcset='/assets/shared/desktop/bg-beta.jpg'
+				/>
+				<source
+					media='(min-width: 768px)'
+					srcset='/assets/shared/tablet/bg-beta.jpg'
+				/>
+				<img src='/assets/shared/mobile/bg-beta.jpg' alt='hero banner' />
+			</picture>
+			<div className={classes.beta__overlay}>
+				<div className={classes.beta__content}>
+					<Accent className={classes.content__accent} />
+					<h1>we're in beta. get your invite today!</h1>
+					<Button variant='black-arrow' className={classes.content__button}>
+						get an invite
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default BetaBanner;
