d6900425c11ed6024cba14f7289b9eb576ea2a4f
chungleee <leon.karho.chung@gmail.com>
2021-05-12 16:31:47 -0400

    add overlay element for easily positioning

--- components/FeaturedStory/FeaturedStory.js
+++ components/FeaturedStory/FeaturedStory.js
@@ -11,17 +11,19 @@ const FeaturedStory = ({ children, data, ...props }) => {
 				<source media='(min-width: 768px)' srcSet={data.images.tablet} />
 				<img src={data.images.mobile} alt={data.title} />
 			</picture>
-			<Container className={classes.container}>
-				<h2>last month's featured story</h2>
-				{children.map((child) => {
-					return (
-						<child.type key={child.props.children}>
-							{child.props.children}
-						</child.type>
-					);
-				})}
-				<Button variant='black-arrow'>read the story</Button>
-			</Container>
+			<div className={classes.overlay}>
+				<Container className={classes.container}>
+					<h2>last month's featured story</h2>
+					{children.map((child) => {
+						return (
+							<child.type key={child.props.children}>
+								{child.props.children}
+							</child.type>
+						);
+					})}
+					<Button variant='black-arrow'>read the story</Button>
+				</Container>
+			</div>
 		</div>
 	);
 };
